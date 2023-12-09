099d31e88cf53f4abf86ac0a85bf6105755db973
Matthew Sasakamoose <matt.sasakamoose@gmail.com>
2023-12-09 16:51:10 -0700

    Added textures

--- main.cpp
+++ main.cpp
@@ -4,8 +4,24 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #include "Shader.h"
 #include "Primitives.h"
+
+float vertices[] = {
+    // positions          // colors           // texture coords
+     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+};
+
+unsigned int indices[] = {
+    0, 1, 3,
+    1, 2, 3
+};
 
 void error_callback(int error, const char* description) {
     std::cerr << "Error: " << description << std::endl;
@@ -47,6 +63,42 @@ int main() {
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    unsigned int VAO, VBO, EBO, texture;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, channels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *data = stbi_load("./../container.jpg", &width, &height, &channels, 0);
+
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cerr << "Failed to load texture" << std::endl;
+    }
+
+    stbi_image_free(data);
+
     int numAttributes;
     glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &numAttributes);
     std::cout << "Maximum Number of Vertex Attributes Supported: " << numAttributes << std::endl;
@@ -66,20 +118,30 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
-        shaderProgram1.use();
+        // shaderProgram1.use();
 
-        float timeValue = glfwGetTime();
-        float redValue = (sin(timeValue + 0.3) / 2.0f) + 0.5f;
-        float greenValue = (sin(timeValue + 0.1) / 2.0f) + 0.5f;
-        float blueValue = (sin(timeValue) / 2.0f) + 0.5f;
+        // float timeValue = glfwGetTime();
+        // float redValue = (sin(timeValue + 0.3) / 2.0f) + 0.5f;
+        // float greenValue = (sin(timeValue + 0.1) / 2.0f) + 0.5f;
+        // float blueValue = (sin(timeValue) / 2.0f) + 0.5f;
 
-        int vertexColorLocation = glGetUniformLocation(shaderProgram1.ID, "color");
-        glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0f);
+        // int vertexColorLocation = glGetUniformLocation(shaderProgram1.ID, "color");
+        // glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0f);
         
-        r.draw();
-        t1.draw();
-        t2.draw();
-        circle.draw();
+        // r.draw();
+        // t1.draw();
+        // t2.draw();
+        // circle.draw();
+
+        shaderProgram2.use();
+
+        glUniform1i(glGetUniformLocation(shaderProgram2.ID, "tex"), 0);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
