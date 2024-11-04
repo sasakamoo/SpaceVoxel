7f2e5ee0e53e8e10bf7c2e12aa1847752d2d7819
Matthew Sasakamoose <matt.sasakamoose@gmail.com>
2024-11-03 21:34:29 -0700

    fix

--- Build/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Moose/Documents/Dev/glm
-# Build directory: C:/Users/Moose/Documents/Projects/GLFWMakefileTest/Build/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- Build/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Moose/Documents/Dev/glm/glm
-# Build directory: C:/Users/Moose/Documents/Projects/GLFWMakefileTest/Build/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
